2b297cf7505cff5e3739eb81f0b9c6e70aea8d63
Raphael <rpanduro.t@gmail.com>
2023-04-27 20:42:01 -0500

    Shape Class

---
+++ Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana2
+{
+    class Shape
+    {
+        public virtual float CalculateArea()
+        {
+            return 0;
+        }
+    }
+}
