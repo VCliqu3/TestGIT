cad15247998e6d34cab4f75a65612247c3823022
Raphael <rpanduro.t@gmail.com>
2023-04-27 20:41:01 -0500

    :)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
