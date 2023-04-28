533f49be6a60bc8b4d1d65da97ff2e1f170e8ba7
Raphael <rpanduro.t@gmail.com>
2023-04-27 20:42:47 -0500

    Triangle

---
+++ Triangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana2
+{
+    class Triangle : Shape
+    {
+        private float b;
+        private float h;
+
+        public Triangle(float b, float h)
+        {
+            this.b = b;
+            this.h = h;
+        }
+
+        public override float CalculateArea()
+        {
+            return b * h / 2;
+        }
+    }
+}
